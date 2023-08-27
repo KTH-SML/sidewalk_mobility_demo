1d69ea3f6cc0d5b196af27463d3830ebdfb1f729
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2023-08-27 15:59:57 +0200

    dev

--- src/aug_demo/scripts/ltms.py
+++ src/aug_demo/scripts/ltms.py
@@ -4,69 +4,52 @@ import numpy as np
 from math import pi
 import rospy
 
-from aug_demo.srv import RefinePath, RefinePathResponse
+from aug_demo.srv import VerifyState, VerifyStateResponse
 from rsu_msgs.msg import StampedObjectPoseArray
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
 
-from odp.shapes import *
-from odp.solver import HJSolver 
-from odp.spect import Grid, SVEA 
-
-
-def load_param(name, value=None):
-    """Function used to get parameters from ROS parameter server
-
-    :param name: name of the parameter
-    :type name: string
-    :param value: default value of the parameter, defaults to None
-    :type value: _type_, optional
-    :return: value of the parameter
-    :rtype: _type_
-    """
-    if value is None:
-        assert rospy.has_param(name), f'Missing parameter "{name}"'
-    return rospy.get_param(name, value)
+# from odp.shapes import *
+# from odp.solver import HJSolver 
+# from odp.spect import Grid, SVEA
 
 class LTMS(object):
+
+    PADDING = 0.3
 
     def __init__(self):
         """Init method for SocialNavigation class."""
 
         rospy.init_node('ltms')
 
-        # Get parameters
-        self.NAME = load_param('~name', 'svea2')
-        self.ORIGIN = load_param('~origin', [0, 0, 0])
+        # grid = Grid('X Y YAW VEL'.split(),
+        #             [+2.0, +2.0, +pi, +1.0],
+        #             [-2.0, -2.0, -pi, +0.3],
+        #             [31, 31, 13, 7],
+        #             [False, False, True, False])
+        # self.grid = grid._grid 
 
-        grid = Grid('X Y YAW VEL'.split(),
-                    [+2.0, +2.0, +pi, +1.0],
-                    [-2.0, -2.0, -pi, +0.0],
-                    [41, 41, 31, 11],
-                    [False, False, True, False])
-        self.grid = grid._grid 
+        # horizon = 2
+        # t_step = 0.2
+        # small_number = 1e-5
+        # self.time_frame = np.arange(start=0, stop=horizon + small_number, step=t_step)
 
-        horizon = 2
-        t_step = 0.2
-        small_number = 1e-5
-        self.time_frame = np.arange(start=0, stop=horizon + small_number, step=t_step)
+        # model_settings = {'uMin': [-pi/5, -0.2],
+        #                   'uMax': [+pi/5, +0.2],
+        #                   'dMin': [0, 0, 0, 0],
+        #                   'dMax': [0, 0, 0, 0]}
 
-        reach_scenario = {'uMode': 'min', 'dMode': 'max'}
-        model_settings = {**reach_scenario, 
-                        'uMin': [-pi/5, -0.2],
-                        'uMax': [+pi/5, +0.2],
-                        'dMin': [0, 0, 0, 0],
-                        'dMax': [0, 0, 0, 0]}
+        # self.model = SVEA(ctrl_range=[model_settings['uMin'],
+        #                               model_settings['uMax']],
+        #                   dstb_range=[model_settings['dMin'],
+        #                               model_settings['dMax']],
+        #                   mode='avoid')
 
-        self.model = SVEA(ctrl_range=[model_settings['uMin'],
-                                      model_settings['uMax']],
-                          dstb_range=[model_settings['dMin'],
-                                      model_settings['dMax']],
-                          mode='reach')
+        # self.solver = HJSolver(self.grid, self.time_frame, self.model)
         
         self.peds_sub = rospy.Subscriber(self.ped_pos_topic, StampedObjectPoseArray, self.peds_cb)
 
-        self.refine_path = rospy.Service('ltms/refine_path', RefinePath, self.refine_path_srv)
+        self.verify_state = rospy.Service('ltms/verify_state', VerifyState, self.verify_state_srv)
         
         rospy.loginfo('Start')
 
@@ -76,67 +59,35 @@ class LTMS(object):
             if obj.object.label == 'person':
                 self.ped_pos.append((obj.pose.pose.position.x, obj.pose.pose.position.y))
 
-    def compute_brs(self, target, constraint):
-
+    def compute_brs(self, target):
         target = np.concatenate([target[..., np.newaxis]] * len(self.time_frame), axis=-1)
-        constraint = np.concatenate([constraint[..., np.newaxis]] * len(self.time_frame), axis=-1)
+        return self.solver(target=target, target_mode='min')
 
-        solver = HJSolver(self.grid, self.time_frame, self.model, 
-                        target=target, target_mode='min',
-                        constraint=constraint, constraint_mode='max')
+    def verify_state_srv(self, req):
 
-        return solver()
+        return VerifyStateResponse(True)
 
-    def refine_path_srv(self, req):
-
-        target = np.zeros(self.grid.shape)
-
-        here_pose = req.path.poses[0]
-        goal_pose = req.path.poses[-1]
-
-        here_point = (here_pose.pose.position.x, here_pose.pose.position.y)
-        goal_point = (goal_pose.pose.position.x, goal_pose.pose.position.y)
-        
-        constraint = np.zeros(self.grid.shape)
-
+        peds = []
         for x, y in self.peds:
-            x += self.ORIGIN[0]
-            y += self.ORIGIN[1]
-            ped = intersect(upper_half_space(self.grid, 0, x + 0.5), lower_half_space(self.grid, 0, x - 0.5),
-                            upper_half_space(self.grid, 1, y + 0.5), lower_half_space(self.grid, 1, y - 0.5))
-            constraint = union(constraint, ped)
-
-        result = self.compute_brs(target, constraint)
-
-        ## TODO:
-        # I should do FRS(poses[0]) intersect BRS(poses[-1])
-
-        point_list = []
-        for i in range(result.shape[-1]):
-            t = req.path.header.stamp + self.time_frame[i]
-            idx = np.unravel_index(result[..., i].argmin(), result.shape)
-
-            if result[idx] > 0:
-                break
-
-            x = idx[0] * self.grid.dx[0] - self.ORIGIN[0]
-            y = idx[1] * self.grid.dx[1] - self.ORIGIN[1]
-
-            point_list.append((t, x, y))
-        
-        path = Path()
-        path.header = req.path.header
-        path.poses = []
-
-        for pose, (t, x, y) in zip(req.path.poses, point_list):
-            if t < pose.header.stamp: 
+            if not self.grid.grid_points[0][0] < x < self.grid.grid_points[0][-1]:
                 continue
-            pose.header.stamp = t
-            pose.pose.position.x = x
-            pose.pose.position.y = y
-            path.poses.append(pose)
+            if not self.grid.grid_points[1][0] < x < self.grid.grid_points[1][-1]:
+                continue
+            ped = intersect(upper_half_space(self.grid, 0, x + self.PADDING), 
+                            lower_half_space(self.grid, 0, x - self.PADDING),
+                            upper_half_space(self.grid, 1, y + self.PADDING), 
+                            lower_half_space(self.grid, 1, y - self.PADDING))
+            peds.append(ped)
 
-        return RefinePathResponse(path)
+        result = self.compute_brs(target=union(*peds))
+        result = -np.minimum(result, axis=-1)
+
+        ix = (self.grid.grid_points[0] - req.state.x).abs().argmin()
+        iy = (self.grid.grid_points[1] - req.state.y).abs().argmin()
+        iyaw = (self.grid.grid_points[2] - req.state.yaw).abs().argmin()
+        ivel = (self.grid.grid_points[3] - req.state.yaw).abs().argmin()
+        
+        return VerifyStateResponse(result[ix, iy, iyaw, ivel] < 0)
 
     def keep_alive(self):
         """
--- src/aug_demo/scripts/vehicle2.py
+++ src/aug_demo/scripts/vehicle2.py
@@ -15,12 +15,14 @@ from svea_mocap.mocap import MotionCaptureInterface
 from svea_social_navigation.track import Arc, Track
 
 # ROS imports
-from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
+import message_filters as mf
+from svea_msgs.msg import VehicleState as VehicleStateMsg
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, TwistStamped, PointStamped
 from sensor_msgs.msg import Joy
 from nav_msgs.msg import Path
-from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 
-from aug_demo.srv import RefinePath
+from aug_demo.srv import VerifyState
 
 def load_param(name, value=None):
     """Function used to get parameters from ROS parameter server
@@ -75,13 +77,35 @@ class Avoider(object):
         self.STATE = load_param('~state', [0, 0, 0, 0])
         self.LOCATION = load_param('~location', 'kip')
 
-        self.refine_path = rospy.ServiceProxy('ltms/refine_path', RefinePath)
+        self.verify_state = rospy.ServiceProxy('ltms/verify_state', VerifyState)
 
         self.rate = rospy.Rate(10)
 
-        self._path_topic = load_param('~path_topic', '/path')
+        self._path_topic = load_param('~path_topic', 'path')
         self._path_pub = rospy.Publisher(self._path_topic, Path, latch=True, queue_size=1)
         self._path_lock = Lock()
+
+        if self.LOCATION == 'sml':
+            self._state_pub = rospy.Publisher('state', VehicleStateMsg, latch=True, queue_size=1)
+            def state_cb(pose, twist):
+                state = VehicleStateMsg()
+                state.header = pose.header
+                state.child_frame_id = 'svea2'
+                state.x = pose.pose.position.x 
+                state.y = pose.pose.position.y
+                roll, pitch, yaw = euler_from_quaternion([pose.pose.orientation.x,
+                                                        pose.pose.orientation.y,
+                                                        pose.pose.orientation.z,
+                                                        pose.pose.orientation.w])
+                state.yaw = yaw
+                state.v = twist.twist.linear.x
+                self._state_pub.publish(state)
+            mf.TimeSynchronizer([
+                mf.Subscriber(f'/qualisys/{self.NAME}/pose', PoseStamped),
+                mf.Subscriber(f'/qualisys/{self.NAME}/velocity', TwistStamped)
+            ], 10).registerCallback(state_cb)
+
+            self._target_pub = rospy.Publisher('target', PointStamped, latch=True, queue_size=1)
 
         # Create Controller
         self.controller = PurePursuitController()
@@ -91,40 +115,56 @@ class Avoider(object):
         self.actuation = ActuationInterface().start()
 
         # Start localization interface based on which localization method is being used
-        self.localizer = (LocalizationInterface().start() if self.LOCATION == 'kip' else
-                          MotionCaptureInterface(self.NAME).start())
+        self.localizer = LocalizationInterface().start()
 
         # Subscribe to joy
         rospy.Subscriber('joy', Joy, self.joy_cb, queue_size=1)
 
         # Initialize vehicle state
-        publish_initialpose(VehicleState(*self.STATE))
-        self.steering = 0.0
-        self.velocity = 0.0
+        # publish_initialpose(VehicleState(*self.STATE))
+        self.steering, self.velocity = 0.0, 0.0
+        self.joy_steering, self.joy_velocity = 0.0, 0.0
         self.state = self.localizer.state
 
         # Instatiate RVIZPathHandler object if publishing to RVIZ
         self.data_handler = RVIZPathHandler()
 
-        rospy.Timer(rospy.Duration(0.2), self.create_path)
+        rospy.Timer(rospy.Duration(0.1), self.create_path)
+
+        self.safe = True
+        def verify_state_tmr(event):
+            if event.last_expected is not None and event.current_real < event.last_expected:
+                return # maybe stupiod
+            self.safe = self.verify_state(self.state._state_msg).ok
+        rospy.Timer(rospy.Duration(0.5), verify_state_tmr)
 
         rospy.sleep(1)
         rospy.loginfo('Start')
 
     def joy_cb(self, msg):
-        steering = 0.5*msg.axes[0] #steering
-        forward = msg.axes[2] # forward
+        steering = 0.5*msg.axes[0]
+        forward = msg.axes[2]
+        backward = msg.axes[1]
         steering_input_max = 0.15
-        velocity_input_max = 1.0
+        velocity_input_max = 0.8
         max_steering = 40*np.pi/180
         max_speed = 1.2
         
         steering = min(steering, steering_input_max)
         steering = max(steering, -steering_input_max)
-        self.velocity = (forward - (-velocity_input_max))*(max_speed)/(velocity_input_max*2)+0
-        self.steering = (steering - (-steering_input_max))*(max_steering*2)/(steering_input_max*2)+(-max_steering)
 
-        rospy.loginfo(f"{self.steering}, {self.velocity}")
+        forward += 1
+        forward /= 2
+        forward *= velocity_input_max
+
+        backward += 1
+        backward /= 2
+        backward *= -velocity_input_max
+
+        self.joy_velocity = forward or backward
+        self.joy_steering = (steering - (-steering_input_max))*(max_steering*2)/(steering_input_max*2)+(-max_steering)
+
+        # rospy.loginfo(f"{self.steering}, {self.velocity}")
 
     def create_path(self, event):
         
@@ -138,11 +178,11 @@ class Avoider(object):
 
         POINT_DENSITY = 50
         BASEWIDTH = 0.32 # [m]
-        HEADWAY = 2 # [s]
+        HEADWAY = 3 # [s]
 
         # steering, velocity = self.rc_remote.steering, self.rc_remote.velocity
-        steering = self.steering
-        velocity = self.velocity
+        steering = self.joy_steering
+        velocity = self.joy_velocity
 
         start_point = (self.state.x, self.state.y, self.state.yaw)
 
@@ -153,7 +193,6 @@ class Avoider(object):
             track = Track([arc], *start_point, POINT_DENSITY=POINT_DENSITY)
             path_from_track = np.array(track.cartesian).T
 
-        # Create array for MPC reference
         path_array = np.zeros((len(path_from_track), 4))
         path_array[:, 0] = path_from_track[:, 0]
         path_array[:, 1] = path_from_track[:, 1]
@@ -165,8 +204,7 @@ class Avoider(object):
         path_msg.header.stamp = rospy.Time.now()
         path_msg.poses = []
 
-        # Idk why it's flipped...
-        for i, (y, x) in enumerate(path_array[:, :2]):
+        for i, (x, y) in enumerate(path_array[:, :2]):
             pose = PoseStamped()
             pose.header.frame_id = path_msg.header.frame_id
             pose.header.stamp = path_msg.header.stamp + i*rospy.Duration(HEADWAY/POINT_DENSITY)
@@ -174,33 +212,14 @@ class Avoider(object):
             pose.pose.position.y = y
             path_msg.poses.append(pose)
 
-        # resp = self.refine_path(path)
-        # path_msg = resp.path
-
         # Publish global path on rviz
         self._path_pub.publish(path_msg)
         
         self.path = path_array
-        
-    def next_target(self):
-        # Get next waypoint index (by computing offset between robot and each point of the path), wrapping it in case of
-        # index out of bounds
-        x0 = (self.state.x, self.state.y)
-        self.distances = np.linalg.norm(self.path[:, 0:2] - x0, axis=1)
-        self.waypoint_idx = self.distances.argmin()
-        while self.waypoint_idx < len(self.distances):
-            if self.THRESHOLD < self.distances[self.waypoint_idx]:
-                break
-            else:
-                self.waypoint_idx += 1
-        else:
-            self.waypoint_idx -= 1
-        self.target = (self.path[self.waypoint_idx, 0], self.path[self.waypoint_idx, 1])
-            
+
     def _visualize_data(self):
         self.data_handler.log_state(self.state)
         self.data_handler.log_ctrl(self.steering, self.velocity, rospy.get_time())
-        self.data_handler.update_target(self.target)
         self.data_handler.visualize_data()
 
     def keep_alive(self):
@@ -225,21 +244,11 @@ class Avoider(object):
         if not self.localizer.is_ready: 
             return
 
-        self.next_target()
-        
-        # If final distance, don't send control signal
-        # Maybe no work, smol hack
-        if self.distances[-1] < self.THRESHOLD:
+        if not self.safe:
             return
-        print(self.target)
-        self.steering, self.velocity = self.controller.compute_control(self.state, self.target)
-
-        # Get optimal velocity (by integrating once the acceleration command and summing it to the current speed) and
-        # steering controls
-        print(f'Control velocity, steering: ({self.velocity:+.02f}, {self.steering:+.02f})')
 
         # Send control to actuator interface
-        self.actuation.send_control(self.steering, self.velocity)
+        self.actuation.send_control(self.joy_steering, self.joy_velocity)
 
         # Visualize data on RVIZ
         self._visualize_data()
